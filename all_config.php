a07a7a5ac729026c3c2db1729f8fb222e12c0753
Hasan Abdullah <47323055+hasan282@users.noreply.github.com>
2021-05-21 13:52:01 +0700

    Add files via upload

---
+++ all_config.php
@@ -0,0 +1,18 @@
+<?php
+
+$Configuration = array(
+
+    // Website Hosting --> true or false
+    'web_hosting' => false,
+
+    // Database Configuration
+    'mysql_host' => 'localhost',
+    'mysql_name' => 'psikotest_new',
+    'mysql_username' => 'root',
+    'mysql_password' => '',
+
+    // Other Configuration
+    'timezone' => 'Asia/Jakarta',
+    'execution_time' => 300
+
+);
